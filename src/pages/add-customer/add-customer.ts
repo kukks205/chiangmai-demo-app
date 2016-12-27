57372d189a5b545e10ae19f0e0dcb7fe4c7c74a6
Satit Rianpit <rianpit@gmail.com>
2016-12-27 16:32:50 +0700

    add camera support

--- src/pages/add-customer/add-customer.ts
+++ src/pages/add-customer/add-customer.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Customer } from '../../providers/customer';
 import * as moment from 'moment';
 
+import { Camera, CameraOptions } from 'ionic-native';
+
 @Component({
   selector: 'page-add-customer',
   templateUrl: 'add-customer.html',
@@ -20,6 +22,8 @@ export class AddCustomerPage {
   firstName: string;
   lastName: string;
   customerTypeId: number;
+  base64Image: string;
+  imageData: string;
 
   constructor(
     public navCtrl: NavController,
@@ -50,18 +54,44 @@ export class AddCustomerPage {
       email: this.email,
       telephone: this.telephone,
       customerTypeId: this.customerTypeId,
-      image: null
+      image: this.imageData
     };
 
     this.customerProvider.saveCustomer(this.token, customer)
       .then((data: any) => {
         if (data.ok) {
-          alert('Success');
+          this.navCtrl.pop();
           }
        }, (error) => {
       
       });
 
+  }
+
+  takePicture() {
+    let options: CameraOptions = {
+      destinationType: 0,
+      sourceType: 1
+    };
+    Camera.getPicture(options).then((imageData) => {
+      this.imageData = imageData;
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  browsePicture() {
+    let options: CameraOptions = {
+      destinationType: 0,
+      sourceType: 0
+    };
+    Camera.getPicture(options).then((imageData) => {
+      this.imageData = imageData;
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
