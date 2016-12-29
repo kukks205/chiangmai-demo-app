da191a4b902cf6d451291f124eb2641fb512d7c7
Satit Rianpit <rianpit@gmail.com>
2016-12-29 14:44:12 +0700

    add chart slide

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -7,28 +7,66 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ChartPage {
   options: any;
+  options2: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.options = {
       chart: {type: 'bar'},
       title: { text: 'simple chart' },
       series: []
-    };
-
-/*
-{
-            name: 'Year 1800',
-            data: [107, 31, 635, 203, 2]
-        }, {
-            name: 'Year 1900',
-            data: [133, 156, 947, 408, 6]
-        }, {
-            name: 'Year 2012',
-            data: [1052, 954, 4250, 740, 38]
-        }
-*/    
+    }; 
     this.options.series.push({ name: 'Year 1800', data: [107, 31, 635, 203, 2] });
-    this.options.series.push({name: 'Year 2012',data: [1052, 954, 4250, 740, 38]});
+    this.options.series.push({ name: 'Year 2012', data: [1052, 954, 4250, 740, 38] });
+    
+    this.options2 = {
+      chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Proprietary or Undetectable',
+                y: 0.2
+            }]
+        }]
+    }
   }
 
   ionViewDidLoad() {
